7660ce9183b981249ce99ba3ea4cac56a1112c36
unknown <osebeckley@gmail.com>
2024-04-26 20:03:55 +0100

    Placehold for preview function

--- medichecks/src/components/Scanner/Scanner.jsx
+++ medichecks/src/components/Scanner/Scanner.jsx
@@ -35,9 +35,6 @@ export default function Scanner() {
 
   }
   return (
- 
-    <section className="take-picture">
-        <button className="scan-btn">scan image</button>
-    </section>
+    <div>placeholder</div>
   )
 }
