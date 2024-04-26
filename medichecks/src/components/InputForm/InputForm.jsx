0c54bfc4b63b0f6f2b7b5234b3c86403023bb23b
David <davefast2003@gmail.com>
2024-04-26 20:41:30 +0100

    extract text button added

--- medichecks/src/components/InputForm/InputForm.jsx
+++ medichecks/src/components/InputForm/InputForm.jsx
@@ -4,38 +4,45 @@ import Scanner from "../Scanner/Scanner";
 import { useState } from "react";
 
 export default function InputForm() {
-    const [file, setFile] = useState(null)
-    const [searchTerm, setSearchTerm] = useState()
+  const [file, setFile] = useState(null);
+  const [searchTerm, setSearchTerm] = useState();
 
-    const handleFileChange = (event) => {
-        const selectedFile = URL.createObjectURL(event.target.files[0]);
-        setFile(selectedFile);
-        console.log(selectedFile)
-    };
+  const handleFileChange = (event) => {
+    const selectedFile = URL.createObjectURL(event.target.files[0]);
+    setFile(selectedFile);
+    console.log(selectedFile);
+  };
 
   return (
     <>
-    <Scanner imagePath = {file}/>
-    <form encType="multipart/form-data" method="post">
-      <label htmlFor="file-upload" className="custom-file-upload">
-        {file? "Uploaded!" : "Upload your image here"}
-        <input id="file-upload" type="file" name="image" accept="image/*" onChange={handleFileChange} />
-        <img src={submitIcon} alt="upload-image" className="submit-icon" />
-      </label>
-      <label htmlFor="search-input" className="custom-file-upload">
-        <input
-          id="search-input"
-          type="text"
-          name="search-term"
-          placeholder="Search a drug"
-        />
-        <img
-          src={searchIcon}
-          alt="enter-search"
-          className="submit-icon search-btn"
-        />
-      </label>
-    </form>
+      <Scanner imagePath={file} />
+      <form encType="multipart/form-data" method="post">
+        <label htmlFor="file-upload" className="custom-file-upload">
+          {file ? "Uploaded!" : "Upload your image here"}
+          <input
+            id="file-upload"
+            type="file"
+            name="image"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+        </label>
+          <button className="extract-text">extract text</button>
+          
+        <label htmlFor="search-input" className="custom-file-upload">
+          <input
+            id="search-input"
+            type="text"
+            name="search-term"
+            placeholder="Search a drug"
+          />
+          <img
+            src={searchIcon}
+            alt="enter-search"
+            className="submit-icon search-btn"
+          />
+        </label>
+      </form>
     </>
   );
 }
