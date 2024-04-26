5768688bd1b6289f14533f3face2a730b441beb3
unknown <osebeckley@gmail.com>
2024-04-26 19:16:59 +0100

    Catch errors

--- medichecks/src/components/Scanner/Scanner.jsx
+++ medichecks/src/components/Scanner/Scanner.jsx
@@ -26,8 +26,13 @@ export default function Scanner() {
       data: form, 
     };
 
-   
-   
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+
   }
   return (
  
