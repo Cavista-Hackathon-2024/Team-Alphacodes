6614c9a67a8f545b383b33e08506be19fd7b4883
David <davefast2003@gmail.com>
2024-04-26 18:24:26 +0100

    image upload input added

--- medichecks/src/App.jsx
+++ medichecks/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Scanner from "./components/Scanner/Scanner";
 import medbotlogo from "./assets/medbotlogo.png";
+import submitIcon from "./assets/uploadimage.png";
 
 function App() {
   return (
@@ -12,6 +13,14 @@ function App() {
 
       <Scanner />
 
+      <form enctype="multipart/form-data" method="post">
+        <label for="file-upload" class="custom-file-upload">
+          <i class="fa fa-cloud-upload"></i> Upload your image here
+          <input id="file-upload" type="file" name="image" accept="image/*" />
+          <img src={submitIcon} alt="submit" id="submit-icon" />
+          
+        </label>
+      </form>
       
     </div>
   );
