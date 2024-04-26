67b9a12e10d54b60c50f22ec35de49eff76f4702
David <davefast2003@gmail.com>
2024-04-26 17:56:53 +0100

    image capture field

--- medichecks/src/App.jsx
+++ medichecks/src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import "./App.css";
+import Scanner from "./components/Scanner/Scanner";
+import medbotlogo from "./assets/medbotlogo.png";
 
 function App() {
-  
-
   return (
-    <div className='App'>
-      <h1>Medichecks</h1>
+    <div className="App">
+      <header>
+        <img src={medbotlogo} alt="medcheck logo" />
+        <h1>Welcome to Medichecks</h1>
+      </header>
+
+      <Scanner />
+
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ medichecks/src/components/Scanner/Scanner.jsx
@@ -0,0 +1,8 @@
+
+export default function Scanner() {
+  return (
+    <section className="take-picture">
+        <button className="scan-btn">scan image</button>
+    </section>
+  )
+}
