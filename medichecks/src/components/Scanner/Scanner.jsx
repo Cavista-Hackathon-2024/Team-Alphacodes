4fd395939c74e3c4b3c420d72a27ed91e5c2e23f
unknown <osebeckley@gmail.com>
2024-04-26 19:05:49 +0100

    Previews image.

--- medichecks/src/components/Scanner/Scanner.jsx
+++ medichecks/src/components/Scanner/Scanner.jsx
@@ -1,6 +1,16 @@
+import axios from 'axios'
+import{ useState } from "react";
 
 export default function Scanner() {
+  const [previewImage, setPreviewImage] = useState("");
+  const [imageFile, setImageFile] = useState({});
+  const [text, setText] = useState("");
+  function handleFileChange(event) {
+    setImageFile(event.target.files[0]);
+    setPreviewImage(URL.createObjectURL(event.target.files[0]));
+  }
   return (
+ 
     <section className="take-picture">
         <button className="scan-btn">scan image</button>
     </section>
